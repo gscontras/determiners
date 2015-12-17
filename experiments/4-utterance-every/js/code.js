b470ebb93ffa1e259a9655ba02e034752b0f6e14
gscontras <scontras@stanford.edu>
2015-12-17 15:19:02 -0800

    added uniqueturker functionality

--- experiments/4-utterance-every/js/code.js
+++ experiments/4-utterance-every/js/code.js
@@ -308,6 +308,16 @@ function make_slides(f) {
 
 /// init ///
 function init() {
+  repeatWorker = false;
+  (function(){
+      var ut_id = "greg-unique-turker-test-15-12-2015";
+      if (UTWorkerLimitReached(ut_id)) {
+        $('.slide').empty();
+        repeatWorker = true;
+        alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
+      }
+  })();
+
   exp.trials = [];
   exp.catch_trials = [];
   exp.critters = _.shuffle(["bird","bug","flower","fish"])
